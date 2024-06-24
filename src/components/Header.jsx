c35739e3f2b8f558033aebb9911ae735c15f7cf9
Joseph Kamil <kamilj@umich.edu>
2024-06-24 03:29:16 -0700

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import logo from '../assets/investment-calculator-logo.png'
+
+export default function APP() {
+    return <header id='header'>
+        <img src={logo} alt='Logo'/>
+        <h1>Investment Calculator</h1>
+    </header>
+}
